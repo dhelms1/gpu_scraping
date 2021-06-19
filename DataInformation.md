643635fda9888d59408960fcac936045ab1757c5
dhelms1 <48806608+dhelms1@users.noreply.github.com>
2021-06-19 07:59:40 -0700

    cleaning started, data dict finished

---
+++ DataInformation.md
@@ -0,0 +1,25 @@
+# Data Set Information
+The following are descriptions for each feature in the data set and their units of measure.
+
+---
+
+- **Page** - Page number from Newegg which the information was taken (*numeric*).
+- **URL** - Link to the GPU that the given observation information is from (*character*).
+- **Brand** - Brand of the given GPU (*character*).
+- **Model** - Model of the given GPU (*character*).
+- **Chipset Manufacturer** - Manufacturer of the Chipster: NVIDIA, AMD, or ATI (*character*).
+- **GPU Series** - Series to which the GPU belongs to (*character*).
+- **GPU** - Specific name for the given GPU (*character*).
+- **Core Clock** - Core clock speed, measured in MHz (*numeric*).
+- **OC Core Clock** - Overclocked Core Clock speed, measured in MHz (*numeric*).
+- **Boost Clock** - Boost Clock speed, measured in MHz (*numeric*).
+- **OC Boost Clock** - Overclocked Boost Clock speed, measured in MHz (*numeric*).
+- **Memory Size** - Size of Memory for GPU, measured in GB (*numeric*).
+- **Memory Interface** - Bit width of the memory bus for the GPU (*character*).
+- **Memory Type* - Type of RAM used within the GPU (*character*).
+- **Month** - Month the GPU became first available (*numeric*).
+- **Year** - Year the GPU became first available (*numeric*).
+- **Price** - Current price at the time of scraping, this is the sale price if on sale (*numeric*).
+- **Shipping** - Cost of shipping at the time of scraping (*numeric*).
+- **AvgRating** - Average Rating for the given GPU, measured on a scale from 0 to 5 (*numeric*).
+- **NumberOfRatings** - Number of ratings that compose the Average Rating score (*numeric*).
--- data_cleaning_script.R
+++ data_cleaning_script.R
@@ -1,8 +1,36 @@
 library(dplyr)
+library(readr)
+library(stringr)
+library(lubridate)
 
 raw.data <- read.csv('data/gpu_raw_data.csv')
 
-raw.data <- raw.data %>% mutate(na.percent=round(rowSums(is.na(.))/ncol(.), 4)*100) %>% filter(na.percent < 40) # 356 -> 314
+raw.data <- raw.data %>% mutate(na.percent=round(rowSums(is.na(.))/ncol(.), 4)*100) %>% 
+  filter(na.percent < 40) %>% select(-na.percent) # 356 -> 314
 
-# Core Clock
-tmp <- data.frame(Core.Clock = raw.data$Core.Clock)
+# Chipset Manufacturer
+raw.data <- raw.data %>% filter(Chipset.Manufacturer == "AMD" | Chipset.Manufacturer == "ATI" | 
+                                  Chipset.Manufacturer == "NVIDIA" | is.na(Chipset.Manufacturer))
+
+# Clock Speeds (FIX THIS, ISSUE WITH MIN/MAX FILLING BOTH REG AND OC CLOCKS)
+raw.data <- raw.data %>% mutate(tmp=regmatches(Core.Clock, gregexpr("[[:digit:]]+", Core.Clock)),
+                                Core.Clock=as.numeric(lapply(tmp, min)),
+                                OC.Core.Clock=as.numeric(lapply(tmp, max))) %>% 
+  select(-tmp) %>% select(Page:Core.Clock, OC.Core.Clock, Boost.Clock:NumberOfRatings)
+
+
+raw.data <- raw.data %>% mutate(tmp=regmatches(Boost.Clock, gregexpr("[[:digit:]]+", Boost.Clock)),
+                                Boost.Clock=as.numeric(lapply(tmp, min)),
+                                OC.Boost.Clock=as.numeric(lapply(tmp, max))) %>% select(-tmp) %>%
+  select(Page:Boost.Clock, OC.Boost.Clock, Memory.Size:NumberOfRatings)
+
+# Memory
+raw.data <- raw.data %>% filter(grepl('GB', Memory.Size)) %>% 
+  mutate(Memory.Size=as.numeric(gsub('GB','', Memory.Size)))
+
+# Month/Year First Available
+
+
+
+
+                      
